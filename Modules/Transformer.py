f331a813b4d8d35bcd70a6c5f383b1d6daf39beb
Ali Rida Awad <ali14.3awad@gmail.com>
2026-04-19 19:36:21 +0300

    Slight modification to Transformer logic + documentation for each transformation.

--- Modules/Transformer.py
+++ Modules/Transformer.py
@@ -1,21 +1,19 @@
 from Modules.DataReader import DataReader
 
 
-# assume input in the form of:
-# inputConf: [k]
-# inputSeries: ['0.0','0.1' ...] basically a list of floats cast into a str
-def ConstantSeries(inputConf: list[str], inputSeries: list[str]) -> list[float]:
+def ConstantSeries(inputConf: list[str], inputSeries: list[list[float]]) -> list[float]:
+    """Outputs a constant series of value k of the same size as the input series."""
     k: float = float(inputConf[0])
-    A: list[float] = list(map(lambda x: float(x), inputSeries))
+    A: list[float] = inputSeries[0]
     B: list[float] = [k] * len(A)
     return B
 
 
-# assume input in the form of:
-# inputSeries: [ ['0.0','0.1' ...], ['0.01','2.3',...] ], list of 2 lists of floats cast into str
-def CrossAbove(inputSeries: list[str]) -> list[int]:
-    A_1: list[float] = list(map(lambda x: float(x), inputSeries[0]))
-    A_2: list[float] = list(map(lambda x: float(x), inputSeries[1]))
+def CrossAbove(inputSeries: list[list[float]]) -> list[int]:
+    """Checks and returns a list of integers representing whether or not 2 input series intersected.
+    1 means they have intersected at the point of its index, 0 means they haven't."""
+    A_1: list[float] = inputSeries[0]
+    A_2: list[float] = inputSeries[1]
     B: list[int] = [0] * len(A_1)
     B[0] = 0
     for t in range(1, len(B)):
@@ -23,13 +21,11 @@ def CrossAbove(inputSeries: list[str]) -> list[int]:
     return B
 
 
-# assume input in the form of:
-# inputConf: [alpha]
-# inputSeries: ['0.0','0.1' ...] basically a list of floats cast into a str
 def ExponentialMovingAverage(
-    inputConf: list[str], inputSeries: list[str]
+    inputConf: list[str], inputSeries: list[list[float]]
 ) -> list[float]:
-    A: list[float] = list(map(lambda x: float(x), inputSeries))
+    """Calculates and outputs a series forming an exponential moving average."""
+    A: list[float] = inputSeries[0]
     B: list[float] = [0.0] * len(A)
     alpha: int = int(inputConf[0])
     for t in range(len(B)):
@@ -40,21 +36,19 @@ def ExponentialMovingAverage(
     return B
 
 
-# assume input in the form of:
-# inputConf: [datasource]
 def Fetch(inputConf: list[str]) -> list[float]:
     """Fetches the series located at the row labeled $datasource$ within the inputConf in fetch_transformation_data.csv"""
     return DataReader().read(dataseries=inputConf[0])
 
 
-# assume input in the form of:
-# inputConf: [balance]
-# inputSeries: [price:[], entry:[], exit:[]] basically a list of floats cast into a str
-def PortfolioSimulation(inputConf: list[str], inputSeries: list[str]) -> list[float]:
+def PortfolioSimulation(
+    inputConf: list[str], inputSeries: list[list[float]]
+) -> list[float]:
+    """Performs a portfolio simulation and outputs the result as a time series."""
     balance = float(inputConf[0])
-    price: list[float] = list(map(lambda x: float(x), inputSeries[0]))
-    entry: list[float] = list(map(lambda x: float(x), inputSeries[1]))
-    exit: list[float] = list(map(lambda x: float(x), inputSeries[2]))
+    price: list[float] = inputSeries[0]
+    entry: list[float] = inputSeries[1]
+    exit: list[float] = inputSeries[2]
     n = len(price)
     positions_held = 0
 
@@ -69,14 +63,15 @@ def PortfolioSimulation(inputConf: list[str], inputSeries: list[str]) -> list[fl
     return portfolio
 
 
-# assume input in the form of:
-# inputConf: [period]
-# inputSeries: ['0.0','0.1' ...] basically a list of floats cast into a str
-def RateOfChange(inputConf: list[str], inputSeries: list[str]) -> list[float]:
-    A: list[float] = list(map(lambda x: float(x), inputSeries))
-    B: list[float] = [0.0] * len(A)
+def RateOfChange(inputConf: list[str], inputSeries: list[list[float]]) -> list[float]:
+    """Calculates the rate of change of a time series over a specified period."""
+    A: list[float] = inputSeries[0]
+    B: list[float] = [float("nan")] * len(A)
     period: int = int(inputConf[0])
     for t in range(len(B)):
+        if t < period:
+            B[t] = float("nan")
+            continue
         if A[t - period] == 0:
             B[t] = float("nan")
         else:
@@ -84,11 +79,11 @@ def RateOfChange(inputConf: list[str], inputSeries: list[str]) -> list[float]:
     return B
 
 
-# assume input in the form of:
-# inputConf: [window]
-# inputSeries: ['0.0','0.1' ...] basically a list of floats cast into a str
-def SimpleMovingAverage(inputConf: list[str], inputSeries: list[str]) -> list[float]:
-    A: list[float] = list(map(lambda x: float(x), inputSeries))
+def SimpleMovingAverage(
+    inputConf: list[str], inputSeries: list[list[float]]
+) -> list[float]:
+    """Calculates and outputs a series forming a simple moving average."""
+    A: list[float] = inputSeries[0]
     B: list[float] = [0.0] * len(A)
     window: int = int(inputConf[0])
     for t in range(len(B)):
